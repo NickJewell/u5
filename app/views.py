25da7cdf64452c5e5ecd7f39669434fefbcaceec
Nick Jewell <nick.jewell@gmail.com>
2014-11-08 22:47:09 +0000

    Added friend-of-friend to basic viz and very basic node CRUD via form

--- app/views.py
+++ app/views.py
@@ -9,7 +9,6 @@ from flask import Flask, Response, json, jsonify, request, Blueprint, render_tem
 import csv
 
 
-
 # Home routing
 
 @app.route('/')
@@ -47,12 +46,14 @@ def unity_viz(account):
 	
 # unity_viz_edges
 # Used to return the edges in JSON format for d3 rendering
+# Updated with friends-of-friends depth via *1..2 in cypher
 # Called indirectly via user_viz.html
 
 @app.route('/unity_viz_edges/<account>')
 def unity_viz_edges(account):
 	intAcc = int(account)
-	results = CypherQuery(graph,"match (p {id: {account} })-[rel :KNOWS]->(f) RETURN p.id, p.first_name, p.last_name, f.id, f.first_name, f.last_name").execute(account=intAcc)
+	#results = CypherQuery(graph,"match (p {id: {account} })-[rel :KNOWS]->(f) RETURN p.id, p.first_name, p.last_name, f.id, f.first_name, f.last_name").execute(account=intAcc)
+	results = CypherQuery(graph, "match (p {id: {account}})-[rel :KNOWS]->(f) WHERE not p=f RETURN p.id as ID1, p.first_name as ID1_first_name, p.last_name as ID1_last_name, f.id as ID2, f.first_name as ID2_first_name, f.last_name as ID2_last_name  UNION match (p {id: {account}})-[rel :KNOWS]->(f)-[rel2 :KNOWS]->(g) WHERE not f=g RETURN f.id as ID1, f.first_name as ID1_first_name, f.last_name as ID1_last_name, g.id as ID2, g.first_name as ID2_first_name, g.last_name as ID2_last_name").execute(account=intAcc)
 	a = {}
 	x = []
 
@@ -65,6 +66,7 @@ def unity_viz_edges(account):
 	z = json.dumps(x, sort_keys=True, indent=4, separators=(',', ': '))
 	
 	return z	
+
 	
 # Unity - as /unity route but with a basic data viz view
 # Renders via: templates/user_viz.html
@@ -91,3 +93,49 @@ def unity_tag_viz_edges(tag):
 	z = json.dumps(x, sort_keys=True, indent=4, separators=(',', ': '))
 	
 	return z	
+	
+@app.route('/create', methods=['GET', 'POST'])
+def create_form():
+
+	return render_template("./create.html")
+
+@app.route('/create_node', methods=['GET', 'POST'])
+def create_node():
+	error = None
+	if 'create' in request.form.values():
+		first =  request.form['first']
+		last  =  request.form['last']
+		grade =  request.form['grade']
+		
+		getMaxID = CypherQuery(graph,"match (p) RETURN max(p.id)").execute()
+		
+		for i in getMaxID:
+			maxID = int(i[0])
+		
+		new_id = maxID + 1
+		
+		creation = CypherQuery(graph, "CREATE (n:Person { id: {new_id}, firstname : {first}, lastname : {last}, grade: {grade} })").execute(new_id=new_id, first=first, last=last, grade=grade)
+		
+	return render_template("./created.html")
+	
+# Vizualising both sides of a node-to-node linkage. 
+# nodes 2 and 8 have data in test set
+# Renders via: templates/user_to_user.html
+@app.route('/edge/<node1>/<node2>')
+def edge(node1, node2):
+	
+	int_node1 = int(node1)
+	int_node2 = int(node2)
+	
+	node1_node2 = CypherQuery(graph, "match (node1 {id:{node1}})-[rel :KNOWS]->(node2 {id:{node2}}) RETURN node1.id, rel.tag, rel.strength, node2.id;").execute(node1=int_node1, node2=int_node2)
+	
+	node2_node1 = CypherQuery(graph, "match (node1 {id:{node2}})-[rel :KNOWS]->(node2 {id:{node1}}) RETURN node1.id, rel.tag, rel.strength, node2.id;").execute(node1=node1, node2=node2)
+	
+		
+	
+	return render_template("user_to_user.html", node1_2=node1_node2, node1=int_node1, node2=int_node2)		
+
+	
+@app.route('/404')
+def err():
+	return render_template("./404.html")
